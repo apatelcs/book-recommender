cfe0b9a46341beede7395bcc292ae69623fce9b2
Abhay Patel <abhaypatel@wireless-10-104-41-115.umd.edu>
2022-07-11 00:21:53 -0400

    Updated collaborative_filtering.py + Added prediction method which will predict a recommendation score for a user/item pair + Commented existing functions ! Need to add data structures to hold book rating scores in sparse matrix

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -5,5 +5,17 @@ def calculate_similarity_avg_diff(item1, item2):
     diff = item1 - item2
     return np.mean(diff)
 
+# Calculates cosine similarity between two arrays
 def calculate_similarity_cosine(item1, item2):
     return np.dot(item1, item2) / (np.linalg.norm(item1) * np.linalg.norm(item2))
+
+# This does not yet work
+# TODO: add a pipeline for dataflow and update prediction function accordingly
+def pred(usr, item, item_ind, item_mat, rating_mat):
+    sim_arr = np.array([calculate_similarity_cosine(item_mat[i], item) for i in range(len(item_mat))])
+    alpha = 1 / np.sum(sim_arr)
+    term = 0
+    for itm in item_mat:
+        term += rating_mat[usr, item_ind] * calculate_similarity_cosine(itm, item)
+
+    return alpha * term
